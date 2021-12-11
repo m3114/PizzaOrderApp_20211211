0b73f1b07c8ef2a01f7aee27be925bca397fb85f
윤성노 <m3114@naver.com>
2021-12-11 12:25:14 +0900

    뷰페이져 작업 마무리 -피이지별 프래그먼트 지정

--- app/src/main/java/com/neppplus/pizzaorderapp_20211211/adepters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/adepters/MainViewPagerAdapter.kt
@@ -3,13 +3,19 @@ package com.neppplus.pizzaorderapp_20211211.adepters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.pizzaorderapp_20211211.fragment.MyProfileFragment
+import com.neppplus.pizzaorderapp_20211211.fragment.PizzaStoreListFragment
 
 class MainViewPagerAdapter( fm:FragmentManager ) : FragmentPagerAdapter(fm) {
-    override fun getCount(): Int {
+    override fun getCount() = 2
 
-    }
+
 
     override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> PizzaStoreListFragment()
+            else -> MyProfileFragment()
+        }
 
     }
 }
