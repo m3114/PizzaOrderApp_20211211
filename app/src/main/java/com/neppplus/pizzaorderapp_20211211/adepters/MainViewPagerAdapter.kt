191c9ef00c6dd5ebeb2543770815d08f1b0da0c0
윤성노 <m3114@naver.com>
2021-12-11 12:22:48 +0900

    뷰페이져 어댑터 기초 작업중 상속-생성자 해결 필수 구현 함수는 작업 필요

---
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/adepters/MainViewPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.neppplus.pizzaorderapp_20211211.adepters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter( fm:FragmentManager ) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+    }
+}
