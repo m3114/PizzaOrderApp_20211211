a5e196342a39cf9f0bf72aa31fb5165417989e25
윤성노 <m3114@naver.com>
2021-12-11 14:53:10 +0900

    실제 가게 데이터 상세화면 표시

--- app/src/main/java/com/neppplus/pizzaorderapp_20211211/ViewStoreDetaliActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/ViewStoreDetaliActivity.kt
@@ -2,8 +2,14 @@ package com.neppplus.pizzaorderapp_20211211
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.neppplus.pizzaorderapp_20211211.datas.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detali.*
 
 class ViewStoreDetaliActivity : AppCompatActivity() {
+
+    lateinit var mStoreData : StoreData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detali)
@@ -14,6 +20,11 @@ class ViewStoreDetaliActivity : AppCompatActivity() {
 
     }
     fun setValues(){
+        mStoreData = intent.getSerializableExtra("store") as StoreData
+        txtStoreName.text = mStoreData.name
+        txtPhoneNum.text = mStoreData.phone
+
+        Glide.with(this).load(mStoreData.logoURL).into(imgLogo)
 
     }
 }
