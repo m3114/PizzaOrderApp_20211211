c69c154cf9c541c58a99c5dba3d2a270a05be828
윤성노 <m3114@naver.com>
2021-12-11 13:37:45 +0900

    겟뷰함수 기초 작업 기본 함수 inflate바로 내보내기

--- app/src/main/java/com/neppplus/pizzaorderapp_20211211/adepters/StoreAdapter.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/adepters/StoreAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.pizzaorderapp_20211211.adepters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.pizzaorderapp_20211211.R
 import com.neppplus.pizzaorderapp_20211211.datas.StoreData
 
 class StoreAdapter(
@@ -9,4 +13,18 @@ class StoreAdapter(
     val resID : Int,
     val mList : ArrayList<StoreData>
 ) : ArrayAdapter<StoreData>(mContext, resID, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+          var tempRow = convertView
+          if (tempRow == null){
+
+              tempRow = mInflater.inflate(R.layout.store_list_item,null)
+
+        }
+        val row = tempRow!!
+
+         return row
+     }
 }
