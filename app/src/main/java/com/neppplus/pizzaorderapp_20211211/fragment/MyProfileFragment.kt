232b4a7bc47b6df0881dc253712caa56490c1a2a
윤성노 <m3114@naver.com>
2021-12-11 12:18:47 +0900

    내 프로필 프래이먼크 추가

---
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/fragment/MyProfileFragment.kt
@@ -0,0 +1,24 @@
+package com.neppplus.pizzaorderapp_20211211.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.pizzaorderapp_20211211.R
+
+class MyProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
