24103db8e9096a10b39596e56dad60728c5b6138
윤성노 <m3114@naver.com>
2021-12-11 13:50:52 +0900

    실제 목록에 가게 이름 반영

--- app/src/main/java/com/neppplus/pizzaorderapp_20211211/adepters/StoreAdapter.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/adepters/StoreAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.neppplus.pizzaorderapp_20211211.R
 import com.neppplus.pizzaorderapp_20211211.datas.StoreData
 
@@ -24,6 +26,16 @@ class StoreAdapter(
 
         }
         val row = tempRow!!
+
+        val data = mList[position]
+        val imgLogo = row.findViewById<ImageView>(R.id.imgLogo)
+        val txtStoreName = row.findViewById<TextView>(R.id.txtStoreName)
+
+        txtStoreName.text = data.name
+
+
+
+
 
          return row
      }
