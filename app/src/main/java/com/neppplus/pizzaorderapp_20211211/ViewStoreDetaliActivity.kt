40f7f55f28e23a6b4e1a2eee36b64bd95636f8d9
윤성노 <m3114@naver.com>
2021-12-11 15:03:11 +0900

    실제 권한 체크 요청

--- app/src/main/java/com/neppplus/pizzaorderapp_20211211/ViewStoreDetaliActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/ViewStoreDetaliActivity.kt
@@ -1,8 +1,14 @@
 package com.neppplus.pizzaorderapp_20211211
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.neppplus.pizzaorderapp_20211211.datas.StoreData
 import kotlinx.android.synthetic.main.activity_view_store_detali.*
 
@@ -17,6 +23,31 @@ class ViewStoreDetaliActivity : AppCompatActivity() {
         setValues()
     }
     fun setupEvents(){
+
+        btnVCall.setOnClickListener {
+            val pl = object  : PermissionListener{
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${mStoreData.phone}")
+                    val myIntent = Intent(Intent.ACTION_CALL,myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(
+                        this@ViewStoreDetaliActivity,
+                        "권한이 거부되어 통화가 불가합니다.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
+
+        TedPermission.create()
+            .setPermissionListener(pl)
+            .setPermissions(Manifest.permission.CALL_PHONE)
+            .check()
 
     }
     fun setValues(){
