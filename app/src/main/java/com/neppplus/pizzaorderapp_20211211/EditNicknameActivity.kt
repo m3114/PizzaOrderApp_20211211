a95abae844f96ff89db7dd295b878167f48dd3a1
윤성노 <m3114@naver.com>
2021-12-11 15:09:38 +0900

    닉네임 변경 화면 추가

---
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/EditNicknameActivity.kt
@@ -0,0 +1,20 @@
+package com.neppplus.pizzaorderapp_20211211
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+        setupEvents()
+        setValues()
+    }
+    fun setupEvents(){
+
+
+    }
+    fun setValues(){
+
+    }
+}
