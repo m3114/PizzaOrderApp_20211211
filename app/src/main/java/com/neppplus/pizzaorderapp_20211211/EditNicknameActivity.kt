5c82b4c26b1bbf32a299bc9e006fe801f1338989
윤성노 <m3114@naver.com>
2021-12-11 15:16:12 +0900

    프래그먼트 결과 가지러 이동, 닉네임 변경 액티비티 완료 누르면 결과 설정. ㅅ복귀

--- app/src/main/java/com/neppplus/pizzaorderapp_20211211/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/EditNicknameActivity.kt
@@ -1,7 +1,9 @@
 package com.neppplus.pizzaorderapp_20211211
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,15 @@ class EditNicknameActivity : AppCompatActivity() {
         setValues()
     }
     fun setupEvents(){
+
+        btnOk.setOnClickListener{
+            val inputNickname = edtNickname.text.toString()
+            val resultsIntent = Intent()
+            resultsIntent.putExtra("nick",inputNickname)
+
+            setResult(RESULT_OK,resultsIntent)
+            finish()
+        }
 
 
     }
--- app/src/main/java/com/neppplus/pizzaorderapp_20211211/fragment/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/fragment/MyProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.neppplus.pizzaorderapp_20211211.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.neppplus.pizzaorderapp_20211211.EditNicknameActivity
 import com.neppplus.pizzaorderapp_20211211.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 
 class MyProfileFragment : Fragment() {
 
@@ -19,6 +22,12 @@ class MyProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        btnEditNickname.setOnClickListener{
+            val myIntent = Intent(requireContext(), EditNicknameActivity::class.java)
+            startActivityForResult(myIntent.REQ_)
+
+        }
     }
 
 }
